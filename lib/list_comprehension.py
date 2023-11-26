9cd13e4a41a83f1737b31ba45ff41f3d40775f62
babu97 <benkaimugul@gmail.com>
2023-11-26 12:18:40 +0000

    finsiehd asssignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    even_numbers = [number for number in num_list if number % 2 == 0]
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamatory_sentences = [sentence + "!" for sentence in sentence_list]
+    return exclamatory_sentences
+
